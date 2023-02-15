e3da6acc7265518ebcc9dfa4dd1b00ab8e70e12b
Miguel Herazo <herazomigue56@gmail.com>
2023-02-15 09:43:41 -0500

    Ejercicios hechos en clase

--- Ejer1.py
+++ Ejer1.py
@@ -71,14 +71,30 @@ class circulo:
         return (2 * math.pi) * radio
 
     def pertenece(self, punto, radio):
+        pass
 
 
 
 #Cree una clase Carta que contenga dos propiedades valor y pinta, las cuales son asignadas solo al momento de la construcción del objeto (se pasan como parámetros en el constructor). Defina 4 constantes que representan los nombres de las 4 pintas que puede tener una carta.
 class Carta:
-    
+
 #Cree una clase CuentaBancaria que contenga los siguientes atributos: numero_cuenta, propietarios y balance. Los tres atributos se deben inicializar en el constructor con valores recibidos como parámetros.
 #Para la clase CuentaBancaria, cree un método depositar que maneje las acciones de depósito en la cuenta.
 #Para la clase CuentaBancaria, cree un método retirar que maneje las acciones de retiro de la cuenta.
 #Para la clase CuentaBancaria, cree un método aplicar_cuota_manejo que aplique un porcentaje del 2% sobre el balance de la cuenta
 #Para la clase CuentaBancaria, cree un método mostrar_detalles que imprima por consola los detalles de la cuenta bancaria.
+class CuentaBancaria:
+
+    def __int__(self, numero_cuenta, propietario, balance):
+        self.numero_cuenta = numero_cuenta
+        self.propietario = propietario
+        self.balance = balance
+
+    def depositar(self):
+
+    def retirar(self):
+
+    def aplicar_cuota_manejo(self,balance):
+      
+
+    def mostrar_detalles(self):
