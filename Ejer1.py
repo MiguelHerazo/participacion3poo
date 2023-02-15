9ed8c1845fcfca1aa3c0c03d3fb5d49041de17a1
Miguel Herazo <herazomigue56@gmail.com>
2023-02-15 09:38:27 -0500

    Ejercicio Circulo

--- Ejer1.py
+++ Ejer1.py
@@ -1,5 +1,7 @@
-
+import math
 #Cree una clase Vehículo que contenga los atributos de instancia velocidad_maxima y kilometraje.
+
+
 
 class vehiculo:
     def __init__(self, velocidad_maxima:float, kilometraje: int):
@@ -39,14 +41,42 @@ class Rectangulo:
         self.y2:int = y2
 
     def perimetro(self,x1,x2,y1,y2):
+        distanciax = (self.x2 - self.x1)
+        distanciay = (self.y2 - self.y1)
+        return (distanciax * 2) + (distanciay * 2)
 
+    def area(self,x1,x2,y1,y2):
+        distanciax = (self.x2 - self.x1)
+        distanciay = (self.y2 - self.y1)
+        return distanciax * distanciay
 
-
+    def es_cuadrado(self,x1,x2,y1,y2):
+        distanciax = (self.x2 - self.x1)
+        distanciay = (self.y2 - self.y1)
+        if distanciax == distanciay:
+            return 'es cuadrado'
+        else:
+            return "no es cuadrado"
 
 #Cree una clase Circulo que tenga las propiedades centro y radio, las cuales se inicializan con parámetros en el constructor. Defina métodos en la clase para calcular el área, el perímetro e indicar si un punto pertenece al círculo o no.
 class circulo:
-    
+    def __init__(self, centro:int, radio:int):
+        self.centro = centro
+        self.radio = radio
+
+    def areaciruculo(self, radio):
+        return math.pi * (radio * radio)
+
+    def perimetro(self,radio):
+        return (2 * math.pi) * radio
+
+    def pertenece(self, punto, radio):
+
+
+
 #Cree una clase Carta que contenga dos propiedades valor y pinta, las cuales son asignadas solo al momento de la construcción del objeto (se pasan como parámetros en el constructor). Defina 4 constantes que representan los nombres de las 4 pintas que puede tener una carta.
+class Carta:
+    
 #Cree una clase CuentaBancaria que contenga los siguientes atributos: numero_cuenta, propietarios y balance. Los tres atributos se deben inicializar en el constructor con valores recibidos como parámetros.
 #Para la clase CuentaBancaria, cree un método depositar que maneje las acciones de depósito en la cuenta.
 #Para la clase CuentaBancaria, cree un método retirar que maneje las acciones de retiro de la cuenta.
